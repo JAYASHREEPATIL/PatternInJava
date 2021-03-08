e6253f25cbaa2f16b72d2a9fec8e8be459eb3b9e
Jayashree Patil <jayapatil9830@gmail.com>
2021-03-08 13:51:47 +0530

    second file added(Bitwise)

---
+++ Bitwise.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Bitwise
+{
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		int n=in.nextInt();
+		int a[]=new int[n];
+		a[0]=1;
+		int k=0;
+		int result=0;
+		int x=n-1;
+		int c=1;
+		for(int i=1;i<n;i++)
+		{
+			a[i]=k;
+			k++;
+		}
+		while(x>=0)
+		{
+			result=result+c*(a[x]);
+			c++;
+			x--;
+		}
+		System.out.println("OP: "+result);
+	}
+}
